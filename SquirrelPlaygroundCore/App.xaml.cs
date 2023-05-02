7a8fe2db93865416fa283be5a32abdf769ee9e32
Niclas Nilsson <ni.nilsson@cgi.com>
2023-05-02 08:07:01 +0200

    Add project files.

---
+++ SquirrelPlaygroundCore/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SquirrelPlaygroundCore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SquirrelAwareApp.HandleEvents(
+            onInitialInstall: OnAppInstall,
+            onAppUninstall: OnAppUninstall,
+            onEveryRun: OnAppRun);
+
+            base.OnStartup(e);
+            //UpdateMyApp().Wait();
+        }
+
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            tools.SetProcessAppUserModelId();
+            // show a welcome message when the app is first installed
+            if (firstRun) MessageBox.Show("Thanks for installing my application!");
+        }
+    }
+}
